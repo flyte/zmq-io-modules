ac1d221ff155d9bf7345ef548f5e202d9273a90a
Flyte <flyte@failcode.co.uk>
2015-06-16 18:15:55 +0100

    Publish tweets over zmq

--- zmq_io/in_twitter.py
+++ zmq_io/in_twitter.py
@@ -1,9 +1,22 @@
 import twitter
 import argparse
+import zmq
 from time import sleep
 from ConfigParser import SafeConfigParser
 
 CONFIG_SECTION = "twitter"
+
+
+def parse_args():
+    """
+    Specify and parse command line arguments.
+    """
+    p = argparse.ArgumentParser()
+    p.add_argument("pub_uri")
+    p.add_argument("track", help="The keyword to filter tweets by")
+    p.add_argument("--config_path", default="twitter.ini")
+    p.add_argument("--pub_prefix", default="TWEET")
+    return p.parse_args()
 
 
 def watch_stream(stream):
@@ -29,16 +42,29 @@ def load_config(path):
         ret[key] = config.get(CONFIG_SECTION, key)
     return ret
 
+
 if __name__ == "__main__":
-    p = argparse.ArgumentParser()
-    p.add_argument("track", help="The keyword to filter tweets by")
-    p.add_argument("--config_path", default="twitter.ini")
-    args = p.parse_args()
+    args = parse_args()
 
     api = twitter.Api(**load_config(args.config_path))
     creds = api.VerifyCredentials()
     print "You are logged in as %s from %s" % (creds.name, creds.location)
 
+    context = zmq.Context()
+    pub = context.socket(zmq.PUB)
+    pub.bind(args.pub_uri)
+
     stream = api.GetStreamFilter(track=args.track.split(","))
     print "Now streaming tweets with the keyword '%s'.." % args.track
-    watch_stream(stream)
+
+    try:
+        while True:
+            for tweet in stream:
+                if "text" in tweet:
+                    print "Publishing new tweet:"
+                    print tweet["text"]
+                    pub.send_string("%s%s" % (args.pub_prefix, tweet["text"]))
+                else:
+                    sleep(0.1)
+    finally:
+        stream.close()
